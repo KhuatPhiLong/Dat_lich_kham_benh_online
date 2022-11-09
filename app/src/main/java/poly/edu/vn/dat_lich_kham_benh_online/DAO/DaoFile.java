a2d776426997e71e892d8eef7a1d1d53f2ebc0aa
KhuatPhiLong <longkhuat0@gmail.com>
2022-11-09 10:54:04 +0700

    lfffdfdasf

--- app/src/main/java/poly/edu/vn/dat_lich_kham_benh_online/DAO/DaoFile.java
+++ app/src/main/java/poly/edu/vn/dat_lich_kham_benh_online/DAO/DaoFile.java
@@ -2,7 +2,10 @@ package poly.edu.vn.dat_lich_kham_benh_online.DAO;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
 
 import poly.edu.vn.dat_lich_kham_benh_online.DTO.DtoFile;
 import poly.edu.vn.dat_lich_kham_benh_online.SQL.MyDbhelper;
@@ -27,7 +30,6 @@ public class DaoFile {
         val.put(DtoFile.colJob,dtoFile.getJob());
         val.put(DtoFile.colEmail,dtoFile.getEmail());
         val.put(DtoFile.colAddress,dtoFile.getAddress());
-
         long res = db.insert(DtoFile.nameTable,null,val);
         return res;
     }
@@ -35,6 +37,59 @@ public class DaoFile {
         String[] check = new String[]{dtoFile.getId()+""};
         int res = db.delete(DtoFile.nameTable,"id = ?",check);
         return res;
+    }
+    public int updateRow(DtoFile dtoFile){
+        ContentValues val = new ContentValues();
+        val.put(DtoFile.colUserId,dtoFile.getUser_id());
+        val.put(DtoFile.colBirthday,dtoFile.getBirthday());
+        val.put(DtoFile.colCccd,dtoFile.getCccd());
+        val.put(DtoFile.colBhyt,dtoFile.getBhyt());
+        val.put(DtoFile.colJob,dtoFile.getJob());
+        val.put(DtoFile.colEmail,dtoFile.getEmail());
+        val.put(DtoFile.colAddress,dtoFile.getAddress());
+        String[] check = new String[]{dtoFile.getId()+""};
+        int res = db.update(DtoFile.nameTable,val,"id = ?",check);
+        return res;
+    }
+    public ArrayList<DtoFile> selectAll(){
+        ArrayList<DtoFile> listFile = new ArrayList<>();
+        Cursor cs = db.query(DtoFile.nameTable,null,null,null,null,null,null);
+        if(cs.moveToFirst()){
+            while(!cs.isAfterLast()){
+                DtoFile dtoFile = new DtoFile();
+                dtoFile.setId(cs.getInt(0));
+                dtoFile.setUser_id(cs.getInt(1));
+                dtoFile.setBirthday(cs.getString(2));
+                dtoFile.setCccd(cs.getString(3));
+                dtoFile.setCountry(cs.getString(4));
+                dtoFile.setBhyt(cs.getString(5));
+                dtoFile.setJob(cs.getString(6));
+                dtoFile.setEmail(cs.getString(7));
+                dtoFile.setCountry(cs.getString(8));
+
+                listFile.add(dtoFile);
+                cs.moveToNext();
+            }
+        }
+        return listFile;
+    }
+    public DtoFile dtoFile(int idFile){
+        DtoFile dtoFile = new DtoFile();
+        String where = "id = ?";
+        String[] whereArgs = {idFile+""};
+        Cursor cs  =db.query(DtoFile.nameTable,null, where,whereArgs,null,null,null);
+        if(cs.moveToFirst()){
+            dtoFile.setId(cs.getInt(0));
+            dtoFile.setUser_id(cs.getInt(1));
+            dtoFile.setBirthday(cs.getString(2));
+            dtoFile.setCccd(cs.getString(3));
+            dtoFile.setCountry(cs.getString(4));
+            dtoFile.setBhyt(cs.getString(5));
+            dtoFile.setJob(cs.getString(6));
+            dtoFile.setEmail(cs.getString(7));
+            dtoFile.setCountry(cs.getString(8));
+        }
+            return dtoFile;
     }
 
 
